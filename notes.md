ffc67b96c6e8a03f617f25510d407d10940bfa40
Julian Bitterlich <bitt.j@protonmail.com>
2026-03-20 11:51:20 +0100

    add notes

---
+++ notes.md
@@ -0,0 +1,5 @@
+# 1.3.2 Function vectors
+
+| can we steer a model to produce different outputs / have a different behaviour, by intervening on the model's forward pass using vectors found by non gradient descent-based methods?
+
+_haven't we just seen that in 1.3.1?_
--- vastai/131_prep.py
+++ vastai/131_prep.py
@@ -19,6 +19,5 @@ with make_connection(settings) as conn:
     )
     conn.run(
         f"cd {settings.remote_repo_dir} && {settings.remote_python} chapter1_transformer_interp/exercises/part31_linear_probes/preparations.py",
-        warn=True,
         hide=False,
     )
